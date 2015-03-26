b7386b8bc568c4d5c262925780dae6425715dfa8
sanjsanj <sanjsanj@hotmail.com>
2015-03-26 14:11:07 +0000

    knows when the game is over and informs you

--- lib/game.rb
+++ lib/game.rb
@@ -6,21 +6,23 @@ class Game
   end
 
   def make_move position
-    # fail 'Game over' if over?
+    opponent.register_shot position
+    return 'Game over' if over?
     switch_turn
   end
 
   def opponent
-    turn == player_1 ? player_2 : player_1
+    @turn == player_1 ? player_2 : player_1
+  end
+
+  def over?
+    # true
   end
 
   private
 
   def switch_turn
-    @turn = opponent # @turn == player_1 ? @turn = player_2 : @turn = player_1
+    @turn = opponent
+    # @turn == player_1 ? @turn = player_2 : @turn = player_1
   end
-
-  # def over?
-  #   true
-  # end
 end
--- spec/game_spec.rb
+++ spec/game_spec.rb
@@ -5,6 +5,10 @@ describe Game do
   let(:player_1) { double :player_1 }
   let(:player_2) { double :player_2 }
 
+  before do
+    allow(player_2).to receive(:register_shot)
+  end
+
   it 'knows who player_1 is' do
     expect(game.player_1).to eq player_1
   end
@@ -22,18 +26,22 @@ describe Game do
     expect(game.turn).to eq player_2
   end
 
-  xit 'doesn\'t switch turns if game is over' do
+  it 'returns game over when the game is over' do
     expect(game).to receive(:over?).and_return true
-    expect { game.make_move :A2 }.to raise_error 'Game over'
+    expect(game.make_move :A2).to eq 'Game over'
   end
 
-  xit 'passes the shot to the opponent' do
+  it 'passes the shot to the opponent' do
     expect(player_2).to receive(:register_shot).with :A1
     game.make_move :A1
   end
 
   it 'knows who the opponent is' do
-    game.turn = player_2
-    expect(game.opponent).to eq player_1
+    expect(game.opponent).to eq player_2
+  end
+
+  it 'knows when the game is over' do
+    # expect(game).to receive(:over?).and_return true
+    expect(game).not_to be_over
   end
 end
