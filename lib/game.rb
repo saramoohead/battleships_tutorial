fdfaace826af8fe33b9a641a56f5fb46b06b5c70
sanjsanj <sanjsanj@hotmail.com>
2015-03-26 13:20:25 +0000

    Knows to raise_error when game is over

--- lib/game.rb
+++ lib/game.rb
@@ -1,7 +1,22 @@
 class Game
-  attr_reader :player_1, :player_2
+  attr_reader :player_1, :player_2, :turn
 
   def initialize player_1, player_2
-    @player_1, @player_2 = player_1, player_2
+    @player_1, @player_2, @turn = player_1, player_2, player_1
+  end
+
+  def make_move #position
+    fail 'Game over' if over?
+    switch_turn
+  end
+
+  private
+
+  def switch_turn
+    @turn == player_1 ? @turn = player_2 : @turn = player_1
+  end
+
+  def over?
+  #   true
   end
 end
--- spec/game_spec.rb
+++ spec/game_spec.rb
@@ -4,7 +4,7 @@ describe Game do
   let(:game) { Game.new(player_1, player_2) }
   let(:player_1) { double :player_1 }
   let(:player_2) { double :player_2 }
-  
+
   it 'knows who player_1 is' do
     expect(game.player_1).to eq player_1
   end
@@ -13,7 +13,21 @@ describe Game do
     expect(game.player_2).to eq player_2
   end
 
-  it 'knows whose turn it is'
-    
+  it 'knows whose turn it is' do
+    expect(game.turn).to eq player_1
+  end
+
+  it 'switches turns after each move' do
+    game.make_move #:A1
+    expect(game.turn).to eq player_2
+  end
+
+  it 'doesn\'t switch turns if game is over' do
+    expect(game).to receive(:over?).and_return true
+    expect { game.make_move }.to raise_error 'Game over'
+  end
+
+  it 'passes the shot to the opponent' do
+
   end
 end
